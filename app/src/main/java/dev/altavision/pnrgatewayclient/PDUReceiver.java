854bfe910e51b5a40dc056122d981e4f50fa8c3e
SpaceSaver <spacesaverundercoverdev@gmail.com>
2023-12-14 23:55:44 -0500

    Incorporate Pypush parsing logic

--- app/src/main/java/dev/altavision/pnrgatewayclient/PDUReceiver.java
+++ app/src/main/java/dev/altavision/pnrgatewayclient/PDUReceiver.java
@@ -14,6 +14,55 @@ import androidx.preference.PreferenceManager;
 
 public class PDUReceiver extends BroadcastReceiver {
     private SharedPreferences mPrefs = null;
+    private static final String TAG = "PDU_RCVR";
+
+    private boolean sanityCheck(String result) {
+        return result != null && result.contains("REG-RESP");
+    }
+
+    private boolean sanityCheck(SmsMessage message) {
+        return message != null && sanityCheck(message.getMessageBody());
+    }
+
+    private String found(String input) {
+        String resdata = input.substring(input.indexOf("REG-RESP"));
+        Log.w(TAG, "PDU: " + resdata);
+        return resdata;
+    }
+
+    private String parse(byte[] pdu) {
+        // Print base64-encoded PDU
+        Log.w(TAG, "PDU: " + new String(Base64.encode(pdu, Base64.DEFAULT)));
+        Log.d(TAG, "Attempting to process message as 3gpp...");
+        try {
+            SmsMessage message = SmsMessage.createFromPdu((byte[]) pdu, "3gpp");
+            if (sanityCheck(message)) {
+                return found(message.getMessageBody());
+            }
+        } catch (Exception e) {
+            Log.e(TAG, "The following error occurred while attempting to process PDU as 3gpp:\n" + e.toString());
+        }
+        Log.d(TAG, "Failed to get body using 3gpp parsing, trying 3gpp2...");
+        try {
+            SmsMessage message = SmsMessage.createFromPdu((byte[]) pdu, "3gpp2");
+            if (sanityCheck(message)) {
+                return found(message.getMessageBody());
+            }
+        } catch (Exception e) {
+            Log.e(TAG, "The following error occurred while attempting to process PDU as 3gpp2:\n" + e.toString());
+        }
+        Log.d(TAG, "Failed to get body using 3gpp2 parsing, manually parsing...");
+        try {
+            String pduString = new String(pdu, 0, pdu.length, StandardCharsets.US_ASCII);
+            if (sanityCheck(pduString)) {
+                return found(pduString);
+            }
+        } catch (Exception e) {
+            Log.e(TAG, "The following error occurred while attempting to process PDU manually:\n" + e.toString());
+        }
+        Log.w(TAG, "PDU could not be deciphered.");
+        return null;
+    }
 
     @Override
     public void onReceive(Context context, Intent intent) {
@@ -46,10 +95,12 @@ public class PDUReceiver extends BroadcastReceiver {
 
                 //messageBody will include the REG-RESP text--i.e.
                 //  REG-RESP?v=3;r=72325403;n=+11234567890;s=CA21C50C645469B25F4B65C38A7DCEC56592E038F39489F35C7CD6972D
-                String messageBody = recMsg.getMessageBody();
+                // String messageBody = recMsg.getMessageBody();
+
+                if 
 
                 //Hands the REG-RESP message off to the SMSReceiver to notify the user
-                SMSReceiver.processResponseMessage(messageBody, context);
+                SMSReceiver.processResponseMessage(parse((byte[]) pdus[i]), context);
 
             }
         }
